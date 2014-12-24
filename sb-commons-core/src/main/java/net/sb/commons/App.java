da1523cf6bf3f794be07ce1fed24607ef8462a86
sergiobelli81 <sergiobelli81@localhost>
2014-12-24 08:39:52 +0100

    [sb] - creazione progetto

---
+++ sb-commons-core/src/main/java/net/sb/commons/App.java
@@ -0,0 +1,13 @@
+package net.sb.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
